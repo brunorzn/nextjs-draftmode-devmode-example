702177d62c6d5e0d31e0ae425596a877115f50ac
Sam Selikoff <sam.selikoff@gmail.com>
2025-12-04 07:57:30 -0500

    Use Suspense to make the draft version of the page non-blocking.

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,41 @@
-"use cache";
-import { draftMode } from "next/headers";
+import { draftMode } from 'next/headers';
+import { Suspense } from 'react';
 
 async function getRandomData(): Promise<number> {
+  await new Promise((resolve) => setTimeout(resolve, 1_000));
   const res = await fetch(
-    "https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new"
+    'https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new'
   );
   const rand = await res.text();
   return parseInt(rand);
 }
 
 export default async function Home() {
+  const { isEnabled } = await draftMode();
+
+  return isEnabled ? (
+    <Suspense fallback="Loading draft content...">
+      <DraftContent />
+    </Suspense>
+  ) : (
+    <PublishedContent />
+  );
+}
+
+async function DraftContent() {
   const data = await getRandomData();
-  const draftModeInstance = await draftMode();
-  if (draftModeInstance.isEnabled) {
-    return (
-      <div>
-        <h1>Draft mode is enabled: {data}</h1>
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <h1>Draft mode is enabled: {data}</h1>
+    </div>
+  );
+}
+
+async function PublishedContent() {
+  'use cache';
+  const data = await getRandomData();
+
   return (
     <div>
       <h1>NO DRAFT MODE: {data}</h1>
